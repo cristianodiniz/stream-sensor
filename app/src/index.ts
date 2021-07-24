300a20f1d3b3d08ae333b9a3703827667c617e25
Cristiano <cristianodp@gmail.com>
2021-07-24 14:15:47 -0300

    save

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,32 +1,54 @@
-import express from 'express';
-import http from "http";
-import { Server } from "socket.io";
 import { BoschCiss } from "./bosh-ciss/ciss";
 import { ISensorData } from './bosh-ciss/model/sensor-data';
+import axios from "axios";
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const NL_SERVER_BASE_ENDPOINT = "http://192.168.130.10:18080/api"
+const NL_SERVER_EMAIL = "cristiano@nautiluslog.com"
+const NL_SERVER_KEY = "7ac600b59bb8e39733c114485050e30380ae979e"
+let NL_SERVER_TOKEN: String = "";
 
-const port = process.env.PORT || 3000;
+const requetToken = () => {
+    return axios.post(NL_SERVER_BASE_ENDPOINT + "/frontend/v1/sessions", {
+        "email": NL_SERVER_EMAIL,
+        "account_key": NL_SERVER_KEY
+    })
+}
 
-app.get('/', (req: any, res: any) => {
-    res.sendFile(__dirname + '/index.html');
-});
+const sendSensorValue = (type: String, payload: any, token: String) => {
 
-app.get('/positions', (req: any, res: any) => {
-    res.sendFile(__dirname + '/positions.json');
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
+    const endpoint = NL_SERVER_BASE_ENDPOINT + "/mobile/v1/genericentities"
+    const body = {
+        "decoration": {
+            "channels": ["channel1", "channel2"],
+            "metadata": [
+                {
+                    "name": "name1",
+                    "value": "value1"
+                },
+                {
+                    "name": "name2",
+                    "value": "value2"
+                }
+            ]
+        },
+        "content": {
+            "imoNumber": 9535163,
+            "teamId": "db6379d4-6c64-4d19-9771-07fd6f9e7aaa",
+            "label": type,
+            "payloadVersion": 1,
+            "payloadType": type,
+            "payload": JSON.stringify(payload)
+        }
+    }
+    const options = {
+        headers: { "X-NAUTI-SESSION": token }
+    }
+    axios.post(endpoint, body, options).then((response) => {
+        console.log("sendSensorValue", response);
+    }, (error) => {
+        console.log("sendSensorValue", error);
     });
-});
-
-server.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+}
 
 const startSensorAtPort = (usbPort: string) => {
     const ciss = new BoschCiss(usbPort);
@@ -34,72 +56,18 @@ const startSensorAtPort = (usbPort: string) => {
         console.log(`on subscribe`);
         const sensorData: ISensorData = data as ISensorData
         console.log(`on subscribe`, data);
-
-        const { accelerationX, accelerationY, accelerationZ } = sensorData
-        if (accelerationX != null && accelerationY != null && accelerationZ != null) {
-            io.emit('bridge/acceleration', {
-                accelerationX,
-                accelerationY,
-                accelerationZ,
-            });
-        }
-
-        const { gyroX, gyroY, gyroZ } = sensorData
-        if (gyroX != null && gyroY != null && gyroZ != null) {
-            io.emit('bridge/gyro', {
-                gyroX,
-                gyroY,
-                gyroZ,
-            });
-        }
-
-        const { humidity } = sensorData
-        if (humidity != null) {
-            io.emit('bridge/humidity', {
-                humidity
-            });
-        }
-
-        const { light } = sensorData
-        if (light != null) {
-            io.emit('bridge/light', {
-                light,
-            });
-        }
-
-        const { magnetometerX, magnetometerY, magnetometerZ } = sensorData
-        if (magnetometerX != null && magnetometerY != null && magnetometerZ != null) {
-            io.emit('bridge/magnetometer', {
-                magnetometerX,
-                magnetometerY,
-                magnetometerZ,
-            });
-        }
-
-        const { noise } = sensorData
-        if (noise != null) {
-            io.emit('bridge/noise', {
-                noise
-            });
-        }
-
-        const { pressure } = sensorData
-        if (pressure != null) {
-            io.emit('bridge/pressure', {
-                pressure,
-            });
-        }
-
-        const { temperature } = sensorData
-        if (temperature != null) {
-            io.emit('bridge/temperature', {
-                temperature,
-            });
-        }
-
-        io.emit('bridge/raw', sensorData);
-
+        handleSendSensorValue("bosch-ciss",data)
     })
+}
+
+
+const handleSendSensorValue = (type: String, payload: any) => {
+    requetToken().then((response) => {
+        const token = response.data.data.token
+        sendSensorValue(type, payload, token)
+    }, (error) => {
+        console.log("requetToken", error);
+    });
 }
 
 startSensorAtPort('ttyACM0');
